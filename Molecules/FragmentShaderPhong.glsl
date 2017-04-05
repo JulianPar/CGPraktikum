78117764161131276e1a52d5cc30d88843ff7cb1
Deathmurd <juecker@students.uni-mainz.de>
2017-04-05 10:26:57 +0200

    FragmentShader zeigt nur noch ein Licht an

--- Molecules/FragmentShaderPhong.glsl
+++ Molecules/FragmentShaderPhong.glsl
@@ -5,8 +5,8 @@ precision mediump float;
 #endif
 
 const vec3 lightPos = vec3(1000.0,1000.0,1000.0); //Kamerakoordinaten
-const vec3 lightPos2 = vec3(-2000.0,0.0,1000.0); //Weltkoordinaten
-const vec4 lp2 = vec4(-100,0,0,1);
+//const vec3 lightPos2 = vec3(-2000.0,0.0,1000.0); //Weltkoordinaten
+//const vec4 lp2 = vec4(-100,0,0,1);
 
 varying vec3 vNormal;
 varying vec3 vPos;
@@ -21,26 +21,26 @@ uniform mat4 uPMat;
 
 void main() {
 
-    vec4 lp2 = uPMat*uMVMat*lp2;
-    vec3 lightPos2 = vec3(lp2);
+    //vec4 lp2 = uPMat*uMVMat*lp2;
+    //vec3 lightPos2 = vec3(lp2);
     vec3 normal = normalize(vNormal);
     vec3 lightDir = normalize(lightPos - vPos);
-    vec3 lightDir2 = normalize(lightPos2 - vPos);
+    //vec3 lightDir2 = normalize(lightPos2 - vPos);
     vec3 reflectDir = reflect(lightDir, normal);
-    vec3 reflectDir2 = reflect(lightDir2, normal);
+    //vec3 reflectDir2 = reflect(lightDir2, normal);
     vec3 viewDir = normalize(-vPos);
 
-    float lambertian = max(dot(lightDir,normal), 0.0)+max(dot(lightDir2,normal), 0.0);;
+    float lambertian = max(dot(lightDir,normal), 0.0)/*+max(dot(lightDir2,normal), 0.0)*/;
     float specular = 0.0;
-    float specular2 = 0.0;
+    //float specular2 = 0.0;
 
     if (lambertian > 0.0) {
        float specAngle = max(dot(reflectDir, viewDir), 0.0);
-       float specAngle2 = max(dot(reflectDir2, viewDir), 0.0);
+       //float specAngle2 = max(dot(reflectDir2, viewDir), 0.0);
        specular = pow(specAngle,uShininess);
-       specular2 = pow(specAngle2,uShininess);
+       //specular2 = pow(specAngle2,uShininess);
     }
 
-    gl_FragColor = vec4(uAmbient + lambertian*uDiffuse + specular*uSpecular + specular2*uSpecular, 1.0);
+    gl_FragColor = vec4(uAmbient * 2.9 + lambertian*uDiffuse * 0.4 + specular*uSpecular * 0.3 , 1.0);
 }
 
