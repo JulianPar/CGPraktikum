541ff5c2d008b9d007a07251507dd2cbae4401e0
Deathmurd <juecker@students.uni-mainz.de>
2017-04-06 10:29:52 +0200

    imort now has structures

--- Molecules/import.cpp
+++ Molecules/import.cpp
@@ -22,116 +22,117 @@ void CGMainWindow::loadHin(){
     std::cout<<"Loading Ice Cube Data"<<std::endl;
 
     std::ifstream instream(fn.toLatin1(),std::ios::binary);
-
+    //put all the lines of the file into 'lines'
     std::vector<std::string> lines;
     if(instream.is_open()){
         while(!instream.eof()){
             std::string st;
             getline(instream,st);
             lines.push_back(st);
-
         }
     }
-
-    int begin = 0;
-    int end = 0;
+    //find the start and end of a molecule
+    std::vector<int> begin;
+    std::vector<int> end;
     for(int i = 0; i< lines.size(); i++){
         std::string temp = lines[i];
         std::cout << temp << "\n";
         if(temp.substr(0,3) == "mol"){
-            begin = i;
-            ogl->m.name = temp;
+            begin.push_back(i);
+            molecule m{temp};
+            ogl->struc.mols.push_back(m);
         }
         if(temp.substr(0,6) == "endmol"){
-            end = i;
+            end.push_back(i);
         }
     }
+     //find out where all the residues of one molecule start and end
+    for(int monster=0;monster<begin.size();monster++){
+        std::vector<int> beginr;//a vector storing all starts of the residues
+        std::vector<int> endr;
+        for(int j = begin[monster]; j < end[monster]; j++){
 
-    std::vector<int> beginr;
-    std::vector<int> endr;
-    for(int j = begin; j < end; j++){
+            std::string temp = lines[j];
+            if(temp.substr(0,3)=="res"){
+                beginr.push_back(j);
 
-        std::string temp = lines[j];
-        if(temp.substr(0,3)=="res"){
-            beginr.push_back(j);
-
-        }
-        if(temp.substr(0,6)=="endres"){
-            endr.push_back(j);
-        }
-     }
-
-    for(int dracula=0;dracula<beginr.size();dracula++){
-        residue r;
-        r.rname=lines[beginr[dracula]];
-        std::cout<<r.rname<<"\n";
-        for(int fumanchu =beginr[dracula];fumanchu<endr[dracula];fumanchu++){
-            std::string temp=lines[fumanchu];
-            if(temp.substr(0,4) == "atom"){
-
-               float i,x,y,z;
-               std::string name;
-               std::istringstream f(temp);
-               std::string str;
-               int len,ind;
-               std::string dummy;
-               int counter =0;
-               std::vector<std::pair<int,std::string>> seanconnery;
-               while (getline(f, str, ' ')) {
-                   if(counter==1){
-                       std::stringstream sstr;
-                       sstr << str;
-                       sstr>>ind;
-                   }
-
-                   if(counter==3){
-                       std::stringstream sstr;
-                       sstr << str;
-                       name=str;
-                   }
-                   if(counter==7){
-                       std::stringstream sstr;
-                       sstr << str;
-                       sstr >> x;
-                   }
-                   if(counter==8){
-                       std::stringstream sstr;
-                       sstr << str;
-                       sstr >> y;
-                   }
-                   if(counter==9){
-                       std::stringstream sstr;
-                       sstr << str;
-                       sstr >> z;
-                   }
-                   /*if(counter==10){
-                       std::stringstream sstr;
-                       sstr<< str;
-                       sstr>>len;
-                   }*/
-                   if(counter>10&&counter%2==1){
-                       std::stringstream sstr;
-                       sstr<<str;
-                       sstr>>len;
-                   }
-                   if(counter>10&&counter%2==0){
-                       std::stringstream sstr;
-                       sstr<<str;
-                       sstr>>dummy;
-                       seanconnery.push_back({len,dummy});
-                       //std::cout<<seanconnery[0].first<<"\n";
-                   }
-                   counter++;
-
-               }
-
-               atom a{ind,name,QVector3D{x,y,z},0.0,seanconnery,QVector3D{0,0,0}};
-               std::cout<<"yesss";
-               r.elements.push_back(a);
             }
+            if(temp.substr(0,6)=="endres"){
+                endr.push_back(j);
+            }
+            for(int dracula=0;dracula<beginr.size();dracula++){
+                residue r;
+                r.rname=lines[beginr[dracula]];
+                std::cout<<r.rname<<"\n";
+                for(int fumanchu =beginr[dracula];fumanchu<endr[dracula];fumanchu++){
+                    std::string temp=lines[fumanchu];
+                    if(temp.substr(0,4) == "atom"){
 
-        }
-    ogl->m.parts.push_back(r);
+                       float i,x,y,z;
+                       std::string name;
+                       std::istringstream f(temp);
+                       std::string str;
+                       int len,ind;
+                       std::string dummy;
+                       int counter =0;
+                       std::vector<std::pair<int,std::string>> seanconnery;
+                       while (getline(f, str, ' ')) {
+                           if(counter==1){
+                               std::stringstream sstr;
+                               sstr << str;
+                               sstr>>ind;
+                           }
+
+                           if(counter==3){
+                               std::stringstream sstr;
+                               sstr << str;
+                               name=str;
+                           }
+                           if(counter==7){
+                               std::stringstream sstr;
+                               sstr << str;
+                               sstr >> x;
+                           }
+                           if(counter==8){
+                               std::stringstream sstr;
+                               sstr << str;
+                               sstr >> y;
+                           }
+                           if(counter==9){
+                               std::stringstream sstr;
+                               sstr << str;
+                               sstr >> z;
+                           }
+                           /*if(counter==10){
+                               std::stringstream sstr;
+                               sstr<< str;
+                               sstr>>len;
+                           }*/
+                           if(counter>10&&counter%2==1){
+                               std::stringstream sstr;
+                               sstr<<str;
+                               sstr>>len;
+                           }
+                           if(counter>10&&counter%2==0){
+                               std::stringstream sstr;
+                               sstr<<str;
+                               sstr>>dummy;
+                               seanconnery.push_back({len,dummy});
+                               //std::cout<<seanconnery[0].first<<"\n";
+                           }
+                           counter++;
+
+                       }
+
+                       atom a{ind,name,QVector3D{x,y,z},0.0,seanconnery,QVector3D{0,0,0}};
+                       std::cout<<"yesss";
+                       r.elements.push_back(a);
+                    }
+                ogl->struc.mols[monster].parts.push_back(r);
+                }
+         }
+
+    }
     }
 
     QString fn2 = QFileDialog::getOpenFileName(this, "Load Ice Cube Data ...", QString(), "txt files (*.txt)" );
@@ -159,19 +160,17 @@ void CGMainWindow::loadHin(){
         std::cout<<"File not found \n";
     }
     std::cout<<"change atom size"<<"\n";
-    for(int i=0;i<ogl->m.parts.size();i++){
-        for(int j=0;j<ogl->m.parts[i].elements.size();j++){
-             //std::cout<<"change atom sizei"<<"\n";
-            for(int k=0;k<vec.size();k++){
-                std::cout<<"i: "<<i<<",j: "<<j<<" k:"<<k<<"\n";
-                std::string nam=ogl->m.parts[i].elements[j].name;
-                if(nam==std::get<0>(vec[k])){
-                    ogl->m.parts[i].elements[j].radius=std::get<1>(vec[k]);
-                    ogl->m.parts[i].elements[j].color=std::get<2>(vec[k]);
-                    //std::cout<<ogl->m.parts[i].elements[j].radius;
-                    //std::cout<<ogl->m.parts[i].elements[j].color.x()<<", "<<ogl->m.parts[i].elements[j].color.y()<<", "<<ogl->m.parts[i].elements[j].color.z()<<"\n";
+    for(int h=0;h<ogl->struc.mols.size();h++){
+        for(int i=0;i<ogl->struc.mols[h].parts.size();i++){
+            for(int j=0;j<ogl->struc.mols[h].parts[i].elements.size();j++){
+                for(int k=0;k<vec.size();k++){
+                    std::cout<<"i: "<<i<<",j: "<<j<<" k:"<<k<<"\n";
+                    std::string nam=ogl->struc.mols[h].parts[i].elements[j].name;
+                    if(nam==std::get<0>(vec[k])){
+                        ogl->struc.mols[h].parts[i].elements[j].radius=std::get<1>(vec[k]);
+                        ogl->struc.mols[h].parts[i].elements[j].color=std::get<2>(vec[k]);
+                    }
                 }
-
             }
         }
     }
